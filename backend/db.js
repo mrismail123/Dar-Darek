38e29693a955e46b16803e77f720163957efd395
ISMAIL OURDAN <ourdanismail666@gmail.com>
2026-02-24 09:43:36 -0800

    create the connection file

---
+++ backend/db.js
@@ -0,0 +1,26 @@
+const mysql = require('mysql2');
+require('dotenv').config(); // This line loads the .env file
+
+// Create the connection pool
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+// Test the connection
+pool.getConnection((err, connection) => {
+    if (err) {
+        console.error('❌ Error connecting to Aiven:', err.message);
+    } else {
+        console.log('✅ Connected to Aiven MySQL successfully!');
+        connection.release();
+    }
+});
+
+module.exports = pool.promise();
